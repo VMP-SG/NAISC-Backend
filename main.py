1d621546145a17a15cc4d448d1cfcae0f7ec7fbc
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-28 15:46:01 +0800

    Add Zone Counter

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, Response, request
 from flask_cors import CORS
+from modules.zone_counter import count_human_gen
+from pathlib import Path
 
 import time
 import itertools
+import numpy as np
+import cv2
 app = Flask(__name__)
 CORS(app)
 
@@ -22,6 +26,28 @@ def stream():
         yield "data: %s %d\n\n" % (c, i)
         time.sleep(.1)  # an artificial delay
     return Response(events(), content_type='text/event-stream')
+
+def video_gen():
+  cap = cv2.VideoCapture('videos/GoldenMile/GoldenMile_1.mp4')
+  while True:
+    success, frame = cap.read()  # read the camera frame
+    if not success:
+      cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+      continue
+    else:
+      ret, buffer = cv2.imencode('.jpg', frame)
+      frame = buffer.tobytes()
+      yield (b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route("/video")
+def video_feed():
+  return Response(video_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/count/zone")
+def zone_stream():
+  if request.headers.get('accept') == 'text/event-stream':
+    return Response(count_human_gen(str(Path.cwd() / "videos" / "GoldenMile" / "GoldenMile_1.mp4")), content_type='text/event-stream')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
