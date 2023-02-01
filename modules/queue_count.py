0ea830ac7dc51072dc8ad1aaa38ea820e70459c9
s1ss9 <85567691+s1ss9@users.noreply.github.com>
2023-02-01 12:17:36 +0800

    Corrected some description

--- modules/queue_count.py
+++ modules/queue_count.py
@@ -2,7 +2,7 @@ def is_store(obj_id):
     return obj_id<100
 
 def queue_count(record):
-    # Note that the same store can be captured by multiple cameras
+    # Note that the same store can be captured by multiple cameras. Take average
     compiled = {}
     for v in record.values():
         for i in range(len(v["zone_people_count"])):
@@ -11,6 +11,6 @@ def queue_count(record):
             else:
                 compiled[v["zone_mapping"][i]] = [v["zone_people_count"][i]]
     compiled = dict(filter(lambda x: is_store(x[0]), compiled.items()))  # Keep only ids corresponding to stores
-    for store_id in compiled:  # Compute mean. Table is considered occupied if mean greater than some threshold
+    for store_id in compiled:  # Compute mean
         compiled[store_id] = sum(compiled[store_id])//len(compiled[store_id])
     return compiled
